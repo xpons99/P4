d03fa51aa6088915d0680c2146bae632dac2995f
Xavier P PAV <xavier.pons.marti@estudiantat.upc.edu>
2022-01-07 20:44:05 +0100

    final verifs

--- scripts/run_spkid.sh
+++ scripts/run_spkid.sh
@@ -200,7 +200,7 @@ for cmd in $*; do
 	   # Perform the final test on the speaker classification of the files in spk_ima/sr_test/spk_cls.
 	   # The list of users is the same as for the classification task. The list of files to be
 	   # recognized is lists/final/class.test
-       compute_lp $db_test $lists/final/class.test
+       compute_$FEAT $db_test $lists/final/class.test
        (gmm_classify -d $w/$FEAT -e $FEAT -D $w/gmm/$FEAT -E gmm $lists/gmm.list $lists/final/class.test | tee class_test.log) || exit 1
 
    elif [[ $cmd == finalverif ]]; then
@@ -210,7 +210,7 @@ for cmd in $*; do
 	   # The list of legitimate users is lists/final/verif.users, the list of files to be verified
 	   # is lists/final/verif.test, and the list of users claimed by the test files is
 	   # lists/final/verif.test.candidates
-       compute_lp $db_test $lists/final/verif.test
+       compute_$FEAT $db_test $lists/final/verif.test
        gmm_verify -d $w/$FEAT -e $FEAT -D $w/gmm/$FEAT -E gmm -w $world lists/final/verif.users lists/final/verif.test lists/final/verif.test.candidates | tee $w/verif_test.log
        perl -ane 'print "$F[0]\t$F[1]\t";
                   if ($F[2] > 0.708306753138503) {print "1\n"}
